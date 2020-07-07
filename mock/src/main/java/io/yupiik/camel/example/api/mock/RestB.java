97600ca5f0a6ae78c541d68f124991f9ba0be297
jbonofre <jbonofre@apache.org>
2020-07-07 08:01:45 +0200

    Add mock service and prepare feature

---
+++ mock/src/main/java/io/yupiik/camel/example/api/mock/RestB.java
@@ -0,0 +1,20 @@
+package io.yupiik.camel.example.api.mock;
+
+import org.osgi.service.component.annotations.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/resta")
+@Component(property = { "osgi.jaxrs.resource=true" })
+public class RestB {
+
+    @Path("/")
+    @Produces("application/json")
+    @GET
+    public String get() {
+        return "foo bar";
+    }
+
+}
