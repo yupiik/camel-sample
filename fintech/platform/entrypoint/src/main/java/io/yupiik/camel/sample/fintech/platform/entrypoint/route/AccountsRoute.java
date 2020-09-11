3b026a01389172bfcff61513bb02c2fb3adf083a
jbonofre <jbonofre@apache.org>
2020-09-11 06:14:21 +0200

    Add OpenAPI feature on CXFRS endpoint

--- fintech/platform/entrypoint/src/main/java/io/yupiik/camel/sample/fintech/platform/entrypoint/route/AccountsRoute.java
+++ fintech/platform/entrypoint/src/main/java/io/yupiik/camel/sample/fintech/platform/entrypoint/route/AccountsRoute.java
@@ -21,7 +21,7 @@ public class AccountsRoute extends RouteBuilder {
 
     @Override
     public void configure() {
-        from("cxfrs://http://0.0.0.0:8282/fintech?resourceClasses=io.yupiik.camel.sample.fintech.platform.entrypoint.api.AccountsEndpoint&providers=provider.jackson")
+        from("cxfrs://http://0.0.0.0:8282/fintech?resourceClasses=io.yupiik.camel.sample.fintech.platform.entrypoint.api.AccountsEndpoint&providers=provider.jackson&features=OpenApiFeature")
                 .id("account-main-route")
                 .log("message received from main entrypoint")
                 .process(new AccountsProcessor())
